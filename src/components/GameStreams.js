e29fc20fa88e9364054c7cec4937dab5107ea34d
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2021-02-25 05:58:59 -0500

    add components folder and initial components

---
+++ src/components/GameStreams.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameStreams = () => {
+	return(
+		<div>
+			GameStreams component
+		</div>
+	)
+}
+
+export default GameStreams;
---
+++ src/components/Games.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Games = () => {
+	return(
+		<div>
+			Games component
+		</div>
+	)
+}
+
+export default Games;
