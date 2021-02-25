6141fbd6107399ea6bb56361b106c1fbffec16bd
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2021-02-25 06:15:21 -0500

    include links to header component, import header comp to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Games from './components/Games'
+import Header from './components/Header'
+import Stream from './components/Streams'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'shards-ui/dist/css/shards.min.css'
@@ -9,10 +11,9 @@ import 'shards-ui/dist/css/shards.min.css'
 function App() {
   return (
 		<Router>
-			<div className="App">
-
-			</div>
+			<Header />
 			<Route exact path='/' component={Games}/>
+			<Route exact path='/top-streams' component={Stream}/>
 		</Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return(
-		<div>
-			Header component
-		</div>
+		<nav className='navbar justify-content-center'>
+			<li className='nav-item nav-link'>
+				<Link to='/'>Top Games</Link>
+			</li>
+			<li className='nav-item nav-link'>
+				<Link to='/top-streams'>Top Live Streams</Link>
+			</li>
+		</nav>
 	)
 }
 
